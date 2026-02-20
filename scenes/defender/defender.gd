159792a45729bd6b87a0d8c582aa590f0a4a2cbd
MikhaelAdam <mikhaeladams737@gmail.com>
2026-02-20 14:34:05 +0700

    added mc

--- scenes/defender/defender.gd
+++ scenes/defender/defender.gd
@@ -1,16 +1,17 @@
 extends Area2D
 class_name Defender
 
-signal die
 
 @export var hp_bar: ProgressBar
 @export var animation_player: AnimationPlayer 
+@onready var has_see: bool = false
+
+func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
 func take_damage(amount: int) -> void:
 	hp_bar.value = max(0, hp_bar.value - amount)
-	if hp_bar.value == 0:
-		die.emit()
-
 
 func get_first_overlapping_body():
 	var bodies = get_overlapping_bodies()
@@ -19,3 +20,9 @@ func get_first_overlapping_body():
 		return first_body
 	else:
 		return null # No bodies are overlapping
+
+func _on_area_entered(area: EnemyScan) -> void:
+	has_see = true
+
+func _on_area_exited(area: EnemyScan) -> void:
+	has_see = false
--- scripts/states/defender/attack.gd
+++ scripts/states/defender/attack.gd
@@ -4,5 +4,5 @@ func enter(previous_state_path: String, data := {}) -> void:
 	defender.animation_player.play("attack")
 
 func physics_update(_delta: float) -> void:
-	if not defender.get_first_overlapping_body():
+	if not defender.has_see:
 		finished.emit(IDLE)
