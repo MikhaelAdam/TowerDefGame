27ebe65ecac8716e54840e73cb061437cd43989e
MikhaelAdam <mikhaeladams737@gmail.com>
2026-02-20 20:37:33 +0700

    working animation for police

--- scenes/defender/defender.gd
+++ scenes/defender/defender.gd
@@ -5,6 +5,7 @@ class_name Defender
 @export var hp_bar: ProgressBar
 @export var animation_player: AnimationPlayer 
 @onready var has_see: bool = false
+@onready var enemy_scaner: EnemyScan
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
@@ -13,16 +14,10 @@ func _ready() -> void:
 func take_damage(amount: int) -> void:
 	hp_bar.value = max(0, hp_bar.value - amount)
 
-func get_first_overlapping_body():
-	var bodies = get_overlapping_bodies()
-	if bodies.size() > 0:
-		var first_body = bodies[0]
-		return first_body
-	else:
-		return null # No bodies are overlapping
-
 func _on_area_entered(area: EnemyScan) -> void:
 	has_see = true
+	enemy_scaner = area
 
 func _on_area_exited(area: EnemyScan) -> void:
 	has_see = false
+	enemy_scaner = null
--- scripts/states/defender/attack.gd
+++ scripts/states/defender/attack.gd
@@ -2,7 +2,12 @@ extends DefenderState
 
 func enter(previous_state_path: String, data := {}) -> void:
 	defender.animation_player.play("attack")
-
+	
 func physics_update(_delta: float) -> void:
 	if not defender.has_see:
+		await defender.animation_player.animation_finished
 		finished.emit(IDLE)
+	if defender.enemy_scaner:
+		defender.enemy_scaner.owner.queue_free()
+func exit() -> void:
+	pass
